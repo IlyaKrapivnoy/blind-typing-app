daafd40feec0f1faa15c61b58e9a142dadfe1d22
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2025-04-06 14:09:43 +0200

    fix: remove Analitics
    exists only for Nuxt, not for Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
 import TypingArea from './components/TypingArea.vue'
 import CursorMask from './components/CursorMask.vue'
-import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <template>
-  <Analytics>
-    <CursorMask  :mask="{ cursor: true, radius: 70 }">
-      <TypingArea />
-    </CursorMask>
-  </Analytics>
+  <CursorMask  :mask="{ cursor: true, radius: 70 }">
+    <TypingArea />
+  </CursorMask>
 </template>
 
 <style scoped>
